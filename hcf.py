cdc6d7e8dd062c9a22505698eb9b4d6ce8cd78be
Mounika <31906993+Monamounika@users.noreply.github.com>
2018-12-15 20:34:56 +0530

    gcd and lcm of 2 nums

--- hcf.py
+++ hcf.py
@@ -5,8 +5,15 @@ if num1"""
 def Gcd(a,b):
     if b==0:
         return a
+
     else:
         return Gcd(b,a%b)
 num1=int(input("please enter num1:"))
 num2=int(input("pls eter num2 :"))
 print(Gcd(num1,num2))
+
+num3=num1*num2
+for i in range(1,num3):
+    if (i%num1==0 and i%num2==0):
+        break
+print("lcm of 2 nums is :",i)
