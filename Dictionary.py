c61a5d57d261cce80a545657fcd19587317b051d
Mounika <31906993+Monamounika@users.noreply.github.com>
2018-12-23 23:33:10 +0530

    Emp info program by using Dic

--- Dictionary.py
+++ Dictionary.py
@@ -1,11 +1,12 @@
 d={}
 n=int(input("enter no of employees: "))
-i=1
-while i<=n:
+for i in (1,n+1):
+#i=1
+#while i<=n:
     name=input("enter employee name :")
     sal=input("enter emp sal:")
     d[name]=sal
-    i=i+1
+ #   i=i+1
 print("Name of employee" , "\t", "sal")
 for x in d:
-    print("\t",x,"\t\t",d[x])
+    print("\t",x,"\t\t\t\t",d[x])
