f301dda2d7fa11c7b431501dac540baf21e5bc73
Mounika <31906993+Monamounika@users.noreply.github.com>
2018-12-26 19:52:17 +0530

    Strings

--- str_sub.py
+++ str_sub.py
@@ -10,10 +10,27 @@ print(course.find("p",1,9))
 print(course.find("p"))
 print(course.index("p"))
 
-'''In find() method if the sun string not found then we'll get -1 where as, in index() method we'll get ValueError '''
+'''In find() method if the sun string not found then we'll get -1 where as, in index() method we'll get ValueError
+ so we can handle this error in app level'''
 s="python programming language"
 #print(s.index("hello"))
 try:
     print(s.index("hello"))
 except ValueError:
     print("not found")
+
+
+s1="java programming language"
+s2=s1.replace("java","python ")
+print(s2)
+print(s1)
+print(id(s1))
+print(id(s2))
+n=s2.split()
+print(n)
+print(type(n))
+for i in n:
+    print(i)
+
+c='-'.join(n)
+print(c)
