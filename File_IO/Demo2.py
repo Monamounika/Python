0b7c0317b00b2f09a09a621af53f0301a9fc7df6
Mounika <31906993+Monamounika@users.noreply.github.com>
2019-01-04 03:39:45 +0530

    WITH keyword

--- File_IO/Demo2.py
+++ File_IO/Demo2.py
@@ -1,3 +1,10 @@
+""" WITH keyword : with is a keyword in python
+    Advantages:
+    It'll TC of closing a file.
+    After completing of all operations automatically even in case of exceptions also and we are not
+    required to close explicitly.
+"""
+
 with open("input1.txt",'w') as f:
     f.write("welcome \n")
     f.write("Python World \n")
